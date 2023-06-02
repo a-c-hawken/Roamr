8a9344fb77501c000fdc772c3be13cb0a32ba612
Aydan Hawken <a.c.hawken@student.saintkentigern.com>
2023-06-02 15:08:49 +1200

    Updated Everything

--- Browser/ViewController.swift
+++ Browser/ViewController.swift
@@ -1,19 +1,59 @@
-//
-//  ViewController.swift
-//  Browser
-//
-//  Created by Aydan H on 30/05/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var newTabButton: UIToolbar!
+    @IBOutlet weak var tabViewButton: UIToolbar!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var menuItems: [UIAction] {
+            return [
+                UIAction(title: "Settings", image: UIImage(systemName: "gearshape"), handler: { (_) in
+                    // Handle the action for the standard item
+                }),
+                UIAction(title: "Extensions", image: UIImage(systemName: "puzzlepiece.extension"), handler: { (_) in
+                    // Handle the action for the standard item
+                }),
+                UIAction(title: "Themes", image: UIImage(systemName: "paintbrush"), handler: { (_) in
+                    // Handle the action for the standard item
+                }),
+                UIAction(title: "Share", image: UIImage(systemName: "square.and.arrow.up"), handler: { (_) in
+                    // Handle the action for the standard item
+                }),
+                UIAction(title: "Split View", image: UIImage(systemName: "square.bottomhalf.filled"), handler: { (_) in
+                    // Handle the action for the standard item
+                }),
+                UIAction(title: "Reload", image: UIImage(systemName: "arrow.clockwise"), handler: { (_) in
+                    // Handle the action for the standard item
+                }),
+                UIAction(title: "Request desktop browsing", image: UIImage(systemName: "desktopcomputer"), handler: { (_) in
+                    // Handle the action for the standard item
+                }),
+                UIAction(title: "Private Mode", image: UIImage(systemName: "eye.slash"), handler: { (_) in
+                    // Handle the action for the standard item
+                }),
+                UIAction(title: "Bookmarks", image: UIImage(systemName: "book"), handler: { (_) in
+                    // Handle the action for the disabled item
+                }),
+                UIAction(title: "History", image: UIImage(systemName: "clock.arrow.circlepath"), handler: { (_) in
+                    // Handle the action for the disabled item
+                }),
+                UIAction(title: "Delete all data", image: UIImage(systemName: "trash"), attributes: .destructive, handler: { (_) in
+                    // Handle the action for the delete item
+                })
+            ]
+        }
+
+        var mainMenu: UIMenu {
+            return UIMenu(title: "", image: nil, identifier: nil, options: [], children: menuItems)
+        }
+        
+        menuButton.menu = mainMenu
     }
-
-
 }
-
