26f22494241e137b0c26926370707d542ca471f2
a-c-hawken <a.c.hawken@student.saintkentigern.com>
2023-07-17 17:33:34 +1200

    Tabs are starting to work and progress, bar with some changes to the theme section of the app that currently don't work.

---
+++ Browser/TabTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TabTableViewCell.swift
+//  Roamr
+//
+//  Created by Aydan Hawken on 17/07/23.
+//
+
+import UIKit
+
+class TabTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
