27feece2400b1dd434b6d276c96b1c5e8c914d4b
a-c-hawken <a.c.hawken@student.saintkentigern.com>
2023-07-18 21:49:38 +1200

    Updated WatchOS app with History

--- WatchRoamr Watch App/ContentView.swift
+++ WatchRoamr Watch App/ContentView.swift
@@ -9,34 +9,59 @@ import SwiftUI
 import AuthenticationServices
 
 struct ContentView: View {
-    @State private var name = " "
+    @State private var name = ""
+    @State var history = [String]()
     
     var body: some View {
-        VStack {
+        HStack{
             TextField("Search", text: $name)
-                .padding()
-            Button("Search", action: signInWithGoogle)
-                .padding()
+                .cornerRadius(90)
+                .frame(height: 50)
+            Button("Go", action: signInWithGoogle)
+                .frame(width: 60, height: 50)
+            }
+        VStack{
+            List(history.reversed(), id: \.self) { item in
+                Text(item)
+                    .cornerRadius(90)
+        }
         }
     }
     
     func signInWithGoogle() {
+        loadHistory()
         let textSearch = name.replacingOccurrences(of: " ", with: "+")
         guard let url = URL(string: "https://www.google.com/search?q=" + textSearch) else { return }
-        
+
         // Initialize an ASWebAuthenticationSession
         let session = ASWebAuthenticationSession(url: url, callbackURLScheme: nil) { callbackURL, error in
-            if let callbackURL = callbackURL {
-            } else if let error = error {
+            if callbackURL != nil {
+                // Handle the callback URL if needed
+            } else if error != nil {
+                // Handle the error if needed
             }
         }
-        
+
         // Start the authentication session
         session.start()
+        saveHistory()
+    }
+    
+    func saveHistory() {
+        history.append(name)
+        let user = UserDefaults.standard
+        user.set(history, forKey: "History")
+    }
+    
+    func loadHistory (){
+        let user = UserDefaults.standard
+        history = user.stringArray(forKey: "History") ?? [String]()
     }
 }
 
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
